ecf48347b1f6821cf4206e46cbe29a8ee080cd9c
Javatlacati <scherzo_16@hotmail.com>
2024-05-10 06:58:22 -0600

    configuración de rutas de imágenes

--- lcdh_app/urls.py
+++ lcdh_app/urls.py
@@ -17,8 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from lcdhsite import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+admin.site.site_header = 'Los Compas de Hierro Mundial'
+admin.site.site_title = 'Los Compas de Hierro Mundial'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='index'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
