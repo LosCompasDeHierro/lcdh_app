32b47b2db84f6f2346ed4f1564f59a57ab480755
Javatlacati <scherzo_16@hotmail.com>
2024-05-10 16:10:31 -0600

    se agrega nombre verboso de la aplicación

--- lcdhsite/apps.py
+++ lcdhsite/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class LcdhsiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'lcdhsite'
+    verbose_name = 'Administración de página principal LCDH Mundial'
