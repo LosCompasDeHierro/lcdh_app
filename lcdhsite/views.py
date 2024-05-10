edd0d53fca5b56f74703a9d534c80c0469c6d8eb
Javatlacati <scherzo_16@hotmail.com>
2024-05-10 06:52:43 -0600

    plantilla y recursos estáticos

--- lcdhsite/views.py
+++ lcdhsite/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
+def home(request):
+    paises = Country.objects.all()
+    anuncios = Anuncio.objects.all()
+    return render(request, 'lcdhsite/home.html', {'logo': 'images/logo.webp', 'paises': paises, 'anuncios': anuncios})
